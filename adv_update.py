ab29c5085b81a3173fcfe5fe318c8b1c6e90f112
KAIF_CODEC <98528577+kaifcodec@users.noreply.github.com>
2025-05-19 16:15:10 +0530

    Delete adv_update.py

--- adv_update.py
+++
@@ -1,9 +0,0 @@
-import os
-os.chdir('/data/data/com.termux/files/home')
-os.system('mv ytconverter bytconverter')
-os.system('git clone https://github.com/kaifcodec/ytconverter.git')
-os.system('rm -r -f $HOME/bytconverter ')
-os.system('pip uninstall pytube')
-os.system('pip install pytube')
-print("\n RESTART YOUR TERMUX APPLICATION AND 'YT Converter' tool.")
-print("\n IF STILL ANY ERROR OCCURS THEN OPEN A ISSUE IN GITHUB")
